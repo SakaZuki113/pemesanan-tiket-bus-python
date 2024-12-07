3f94bb6d4401e855148db2ac19595384722b3c88
SakaZuki113 <sakazukiindarto491@gmail.com>
2024-12-07 18:03:55 +0700

    Nambah koneksi.py

---
+++ src/koneksi.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def koneksi_db():
+    db = sqlite3.connect("Booking_Bus.db")
+    cursor = db.cursor()
+    return db, cursor
+
+
+def create_tables():
+    db, cursor = koneksi_db()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nama VARCHAR(255),
+        email VARCHAR(255) UNIQUE,
+        password VARCHAR(255),
+        status_logged INTEGER DEFAULT 0 CHECK (status_logged IN (0, 1))
+    )
+    ''')
+    db.commit()
+
+create_tables()
