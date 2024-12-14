1ef335e4069821ed022e7cbb0a2bf6f1df05e43e
SakaZuki113 <sakazukiindarto491@gmail.com>
2024-12-14 15:28:58 +0700

    Perubahan pada function statement, dan array isiData

--- src/dashboard/halaman_user.py
+++ src/dashboard/halaman_user.py
@@ -1,16 +1,12 @@
 from src.auth.login import login
 from time import sleep
 
-def dashboard(isiData = None):
+def dashboard(isiData = {}):
     if not isiData:
         print("Kamu belum login! silahkan login dulu dong...")
         print("Redirect 3 seconds...")
         sleep(3)
         login()
     else:
-        name = isiData[1]
+        name = isiData[0]['nama']
         print(f"Selamat datang {name}!")
-
-
-# if __name__ == "__main__":
-#     print("Tidak bisa menjalankan function ini. Silahkan langsung ke file main.py")
